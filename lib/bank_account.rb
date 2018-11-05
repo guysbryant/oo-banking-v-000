626fea17ff8fb7b96842a52fe84be4d462212351
Guy Bryant <guy.s.bryant@gmail.com>
2018-11-05 01:24:43 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,22 @@
 class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
+  
   def initialize(name)
     @name = name
     @balance = 1000
     @status = 'open'
+  end
+  
+  def deposit(amount)
+    @balance += amount
+  end
+  
+  def display_balance
+    @balance
+  end
+  
+  def valid?
+    @balance > 0 && status == 'open'
   end
 end
