8f0c01c52600b6b9dc458a6ca2aba73b83ef601f
Guy Bryant <guy.s.bryant@gmail.com>
2018-11-05 01:37:23 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,10 @@ class Transfer
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
+    else 
+      @status = "Transaction rejected. Please check your account balance."
     end
+    
   end
   
 end
